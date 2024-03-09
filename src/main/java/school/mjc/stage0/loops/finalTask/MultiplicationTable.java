f84a9229d75fc6150b4112ff9020b9cd863d2fc8
Marcos <msoto@hopewellsystem.com>
2024-03-09 00:45:19 -0600

    M4 Loops TF 2

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,18 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
-    }
+	public void printTable(int numberTableToPrint) {
+		if (numberTableToPrint >= 0) {
+			for (int i = 1; i <= 10; i++) {
+				int res = i * numberTableToPrint;
+				System.out.println(i + " x " + numberTableToPrint + " = " + res);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		MultiplicationTable multi = new MultiplicationTable();
+		multi.printTable(0);
+	}
 }
